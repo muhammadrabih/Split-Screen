a83d6ac419d3ee01b357b9cd0c31e8106420b05a
Muhammad Rabih <82717021+muhammadrabih@users.noreply.github.com>
2023-02-18 00:06:02 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const right = document.querySelector(".right .hotspot");
+const left = document.querySelector(".left .hotspot");
+
+left.addEventListener("mouseenter", () =>
+	container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+	container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+	container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+	container.classList.remove("hover-right")
+);
